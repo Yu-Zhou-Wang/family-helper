135bddbe9957ff5f32c1d469df21aa8cca74ce07
Yu-Zhou-Wang <yuzhouwa@andrew.cmu.edu>
2023-07-06 11:22:33 +0800

    Added Authorization

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "familyhelpere0864d06": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsConfig from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Amplify</h1>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main style={{ textAlign: 'center' }}>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
